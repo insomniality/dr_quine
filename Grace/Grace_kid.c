fa5e5031b217cd3ebb8b518905cb2a1cbab03cb9
Michael Arzumanyan <michaaelarzumanyan@gmail.com>
2025-04-27 03:39:03 +0400

    6

--- Grace/Grace_kid.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#define NAME "Grace_kid.c"
-#define STR "#include <stdio.h>%c#define NAME %cGrace_kid.c%c%c#define STR %c%s%c%c#define GOD	int main(){char * s = STR; FILE * fd = fopen(NAME, %cw%c);if(fd > 0) fprintf(fd, s, 10, 34, 34, 10, 34, s, 34, 10, 34, 34, 10, 10, 10, 10, 10);fclose(fd);}%c/*%c	jon uwu%c*/%cGOD%c"
-#define GOD	int main(){char * s = STR; FILE * fd = fopen(NAME, "w");if(fd > 0) fprintf(fd, s, 10, 34, 34, 10, 34, s, 34, 10, 34, 34, 10, 10, 10, 10, 10);fclose(fd);}
-/*
-	jon uwu
-*/
-GOD
